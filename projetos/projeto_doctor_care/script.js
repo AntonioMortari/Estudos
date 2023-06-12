7cf032c0570a0fa8e159ae07057da18b86313422
Antonio Mortari <113060294+AntonioMortari@users.noreply.github.com>
2023-06-12 12:15:27 -0300

    attD

--- projetos/projeto_doctor_care/script.js
+++ projetos/projeto_doctor_care/script.js
@@ -6,11 +6,22 @@ window.addEventListener('scroll', function(){
 
 //Menu hamburguer
 
-var btn_mobile = document.getElementById('btn-mobile')
+var btn = document.getElementById('btn-open')
+var header = document.getElementById('section1-header')
 
 function toggleMenu(){
-    var nav = document.getElementById('section1-nav')
-    nav.classList.toggle('active')
+    var menuM = document.getElementById('menu-mobile')
+    menuM.classList.toggle('active')
+    if(menuM.classList.contains('active')){
+        btn.src = 'img/botao-fechar.png'
+        header.style.padding = '18px 0px'
+        header.style.borderBottom = '3px solid #003129'
+    }else{
+        btn.src = 'img/cardapio.png'
+        header.style.padding = '10px'
+        header.style.borderBottom = 'none'
+    }
 }
 
-btn_mobile.addEventListener('click', toggleMenu)
+btn.addEventListener('click', toggleMenu)
+
