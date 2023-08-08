5bd609d03ea634e3e807dd24df196dd1068fd6ec
Antonio Mortari <113060294+AntonioMortari@users.noreply.github.com>
2023-08-08 19:26:39 -0300

    ok

---
+++ python/cursoEmVídeo/desafios/realParaInteiro.py
@@ -0,0 +1,5 @@
+import math
+n = float(input("Digite um número: "))
+inteiro = math.ceil(n)
+
+print("O número digitado arredondado para cima é {}".format(inteiro))
---
+++ python/cursoEmVídeo/desafios/sorteioAluno.py
@@ -0,0 +1,7 @@
+import random
+print("="*20)
+print("NOME DOS ALUNOS")
+print("1 - João  2 - Maria 3 - Marcos 4 - Felipe")
+n = random.randint(1,4)
+
+print("O aluno sorteado foi o {}".format(n))
